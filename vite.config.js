0fced5cd4255d9364bd07015590f47651e8b1523
roberto_ar <robertolejandroatilanoarias@gmail.com>
2025-04-11 02:14:37 -0600

    change title page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/learning-react/projects/02-conecta-4/'
+  base: '/conecta-4-react-app/'
 })
